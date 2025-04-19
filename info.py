a55f2711ea643e546e8c9facc1c4cda946528857
Aissx <achouri.aissa@univ-guelma.dz>
2025-04-19 18:52:12 +0100

    Update bot token in info.py

--- info.py
+++ info.py
@@ -4,7 +4,7 @@ from telebot.types import Message
 from telebot.types import InlineKeyboardMarkup
 from telebot.types import InlineKeyboardButton
 
-token="YOUR_BOT_TOKEN" # Your bot token
+token="7968323217:AAH0QGNvqKuxwDVsLUDURvei_tU2GeRyBjw" # Your bot token
 
 
 bot=telebot.TeleBot(token)
